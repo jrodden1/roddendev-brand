b18cb02582a39e2ea843ffdbd93161840d12a7b3
Jeremiah Rodden <jrodden1@gmail.com>
2019-11-26 19:01:07 -0700

    Created components for svgs in 'why' section

---
+++ src/components/svg/CreatorAndFixer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CreatorAndFixer = () => {
+   return (
+      <svg width="175" height="125" viewBox="0 0 321 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <path d="M77.7106 10.1581L10.72 77.1414L77.7106 144.125" stroke="#FFF" strokeWidth="20" strokeLinecap="round" strokeLinejoin="round"/>
+         <path d="M243.729 10.158L310.72 77.1413L243.729 144.125" stroke="#FFF" strokeWidth="20" strokeLinecap="round" strokeLinejoin="round"/>
+         
+         <g clipPath="url(#clip0)">
+            <path stroke="#FFF" strokeWidth="3" fill="#5b5b5b" d="M203.907 69.1622C211.467 57.7722 210.532 43.1929 202.663 32.9097L189.009 53.4797C187.542 55.6897 184.572 56.29 182.361 54.8232L173.35 48.8432C171.093 47.3859 170.493 44.4162 172.007 42.1966L185.661 21.6266C173.09 18.4268 159.385 23.2084 151.824 34.5984C144.828 45.1384 145.11 58.4269 151.413 68.487L121.472 113.594C117.597 119.43 119.179 127.255 125.007 131.082C130.844 134.956 138.67 133.374 142.497 127.547L172.335 82.6575C184.243 84.7647 196.797 79.8722 203.907 69.1622Z" />
+         </g>
+         
+         <defs>
+            <clipPath id="clip0">
+               <rect width="115.421" height="115.432" fill="white" transform="matrix(0.198146 0.980173 -0.980181 0.198104 206.104 9.39859)"/>
+            </clipPath>
+         </defs>
+      </svg>
+   );
+}
+
+export default CreatorAndFixer;
